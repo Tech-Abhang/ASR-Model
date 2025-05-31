69b742253841204c2125f63d2182db07011b6a63
Abhang Pawar <abhangpawar03@gmail.com>
2025-05-31 23:21:20 +0530

    readme and added testing data

--- huggingface_client.py
+++ huggingface_client.py
@@ -9,5 +9,5 @@ client = InferenceClient(
     api_key=os.getenv("HUGGINGFACE_TOKEN"),
 )
 
-output = client.automatic_speech_recognition("8975-270782-0114.flac", model="openai/whisper-large-v3")
+output = client.automatic_speech_recognition("test.flac", model="openai/whisper-large-v3")
 print(output)
